a6ca94bfdbc098b8ac21c5bcbf8a4f6bc773a526
Marek Cmero <cmerom@gmail.com>
2020-05-04 15:34:04 +1000

    Added Leucegene helper methods and normals controls test results.

---
+++ code/leucegene_helper.R
@@ -0,0 +1,25 @@
+collate_vartypes <- function(results) {
+    # take results from MINTIE and collate
+    # vartypes into the following classes:
+    # fusion, NSV, TSV and unknown
+
+    # categories
+    fusions <- c("FUS")
+    splicevars <- c("AS", "EE", "NE", "NEJ", "PNJ", "RI")
+    tsvs <- c("DEL", "INS")
+
+    results$class <- "Unknown"
+    results$class[results$variant_type %in% fusions] <- "Fusion"
+    results$class[results$variant_type %in% tsvs] <- "Transcribed structural variant"
+    results$class[results$variant_type %in% splicevars] <- "Novel splice variant"
+
+    # fix variants marked as fusions as TSVs if they
+    # are fusions (i.e. hard-clips) within the same gene
+    is_tsv <- results$overlapping_genes %>% str_split(":|\\|") %>%
+                lapply(., duplicated) %>%
+                lapply(., any) %>%
+                unlist()
+    results$class[is_tsv] <- "Transcribed structural variant"
+
+    return(results)
+}
