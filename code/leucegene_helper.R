d8d5b9674ab37d849c418d03aeb63df57a80556d
Marek Cmero <cmerom@gmail.com>
2020-05-07 12:09:08 +1000

    Added Leucegene variant validation
    - convenience functions for finding
      variants in MINTIE results

--- code/leucegene_helper.R
+++ code/leucegene_helper.R
@@ -27,11 +27,56 @@ collate_vartypes <- function(results) {
 get_samples_with_kmt2a_sv <- function(results, controls) {
     # return unique samples with KMT2A hard/soft
     # clipped variant transcripts
-    samples <- results %>%
-                filter(controls == controls,
-                       variant_type %in% c("FUS", "UN"),
-                       overlapping_genes %like% "KMT2A") %>%
+    con_results <- filter(results, controls == controls)
+    get_samples_with_variant(con_results, "KMT2A", "PTD") %>%
+        return()
+}
+
+get_varfilter <- function(variant_type) {
+    # return what MINTIE might classify
+    # the input variant type as
+    varfilter <- NULL
+    if (variant_type == "fusion") {
+        varfilter <- c("FUS", "UN")
+    } else  if (variant_type == "ITD") {
+        varfilter <- c("INS", "FUS", "UN")
+    } else if (variant_type == "PTD") {
+        varfilter <- c("FUS", "UN")
+    }
+    return(varfilter)
+}
+
+get_gene_hits <- function(results, gene) {
+    # return rows from MINTIE results
+    # containing variant gene
+    if (gene == "") {
+        return(data.frame())
+    }
+    vargenes <-  results$overlapping_genes %>%
+        str_split("\\||:")
+    gene_hit <- lapply(vargenes, function(x) any(x %in% gene)) %>%
+        unlist()
+    return(results[gene_hit,])
+}
+
+get_samples_with_variant <- function(results, gene, variant_type) {
+    # return unique samples with variant from MINTIE's results
+    varfilter <- get_varfilter(variant_type)
+    samples <- get_gene_hits(results, gene) %>%
+                filter(variant_type %in% varfilter) %>%
                 pull(sample) %>%
                 unique()
     return(samples)
+}
+
+is_variant_in_sample <- function(srx_id, gene1, gene2, vartype, results) {
+    # checks if variant is in MINTIE output for the given
+    # sample, genes and variant type
+    varfilter <- get_varfilter(vartype)
+    hits <- filter(results, sample == srx_id) %>%
+                filter(variant_type %in% varfilter)
+    hits_gene1 <- get_gene_hits(hits, gene1)
+    hits_gene2 <- get_gene_hits(hits, gene2)
+
+    return(nrow(hits_gene1) > 0 | nrow(hits_gene2) > 0)
 }
