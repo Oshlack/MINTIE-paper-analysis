42ce21bf9eb016c54d1b9526b66080d0e9a954b2
Marek Cmero <cmerom@gmail.com>
2020-07-03 15:32:29 +1000

    Added CICERO results

--- code/plot.R
+++ code/plot.R
@@ -15,14 +15,14 @@ plot_simu_benchmarking <- function(results, method_name) {
 plot_all_simu_benchmarks <- function(results) {
     # plot bar chart results of variants found
     # for all methods
-    ggplot(results, aes(vartype, value, group=method, fill=class)) +
+    ggplot(results, aes(vartype, value, group = method, fill = class)) +
         geom_bar(stat="identity", position="dodge") +
         theme_bw() +
         theme(axis.text.x = element_text()) +
-        facet_wrap(~method, nrow=2) +
+        facet_wrap(~method, nrow = 3) +
         coord_flip() +
         ylab("variants found") +
         xlab("") +
-        scale_fill_manual(values=cols) +
+        scale_fill_manual(values = cols) +
         theme(legend.position = "bottom")
 }
